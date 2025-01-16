539e70d41a04106565ef0617a328e1e9c378071a
Lukasz Ponski <lponski@yahoo.com>
2025-01-16 19:32:13 +0100

    [w_info] Add temperature functions

--- weather_info/src/lib.rs
+++ weather_info/src/lib.rs
@@ -1,14 +1,67 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+use anyhow::Context;
+use open_meteo_rs::forecast::ForecastResult;
+use std::error::Error;
+
+extern crate open_meteo_rs;
+
+pub struct GPScoordinates {
+    pub longitude: f64,
+    pub latitude: f64,
+}
+
+#[derive(Debug)]
+pub struct CurrentTemperature {
+    pub unit: String,
+    pub value: f32,
+}
+
+async fn get_forecast_result(gps_coordinates: GPScoordinates) -> Result<ForecastResult, Box<dyn Error>> {
+    let client = open_meteo_rs::Client::new();
+    let mut opts = open_meteo_rs::forecast::Options::default();
+
+    (opts.location.lat, opts.location.lng) = (gps_coordinates.latitude, gps_coordinates.longitude);
+
+    opts.current.push("temperature_2m".into());
+
+    client.forecast(opts).await
+}
+
+pub async fn get_current_temperature(gps_coordinates: GPScoordinates) -> Result<CurrentTemperature, Box<dyn Error>> {
+    let forecast_current = 
+        get_forecast_result(gps_coordinates)
+            .await?
+            .current
+            .with_context(|| "forecast current failed")?;
+
+    let forecast_temp = 
+        forecast_current
+            .values
+            .get("temperature_2m")
+            .with_context(|| "temperature failed")?
+        ;
+
+    let temperature = CurrentTemperature {
+        unit: forecast_temp.unit.clone()
+            .with_context(|| "temperature unit cloning failed")?,
+        value: forecast_temp.value.to_string().parse::<f32>()
+            .with_context(|| "parsing temperature failed")?,
+    };
+
+    Ok(temperature)
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    #[tokio::test]
+    async fn it_works() {
+        let gps = GPScoordinates {
+            latitude: 51.76,
+            longitude: 19.65,
+        };
+        let result =  get_current_temperature(gps).await.unwrap();
+        println!("[result] {:#?}", result);
+        assert!(true);
     }
 }
